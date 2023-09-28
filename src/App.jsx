8bf3a47094da2bdd2cfa87be467770550d6121e7
jonas4789 <johangonzalez790@fpuna.edu.py>
2023-09-28 18:14:53 -0400

    cambio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import MyRoutes from "./routes"
+
+function App() {
+
+  return (
+    <>
+      <MyRoutes/>
+    </>
+  )
+}
+
+export default App
---
+++ src/views/Inicio/index.jsx
@@ -0,0 +1,39 @@
+import './../../App.css';
+import { useState, useEffect } from 'react';
+import Login from './Login';
+import jwtDecode from 'jwt-decode';
+
+
+function Inicio() {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('accessToken')
+    if (token){
+      setUserId(jwtDecode(JSON.parse(token)).user_id)
+    }
+  }, []);
+
+  const onLoginHandler = (userId) => {
+    console.log(`Codigo de usuario: ${userId}`)
+    setUserId(userId)
+  };
+
+  const onLogoutHandler = () => {
+    setUserId(null)
+    window.localStorage.removeItem('accessToken')
+  };
+
+
+  return (
+    <>
+      {userId ? (
+        <Login onLogout={onLogoutHandler} userId={userId} /> 
+      ) : (
+        <Login onLogin={onLoginHandler} />
+      )}
+    </>
+  );
+}
+
+export default Inicio;
